8ace53145a99894715258fe3d54330dd372cb522
Nicolas Parrado <nparrado@sentra.cl>
2021-02-01 21:00:24 -0300

    Se incluye .env para que cargue la info desde la variable de ambiente

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 docker build -t springapp .
 docker network create mired
 docker run --name=db -e MYSQL_DATABASE=bdtest -e MYSQL_ROOT_PASSWORD=123456 --network=mired -d mariadb
-docker run --name=springc -p 8080:8080 --network=mired -d springapp
+docker run --name=springc -p 8080:8080 --network=mired --env-file=.env -d springapp
 
